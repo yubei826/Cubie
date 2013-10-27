93e395f9e7fb3cf6f3c7efbd6c9f1894f36af246
Zawa <eng.jhl@gmail.com>
2013-10-27 14:32:19 +0800

    update

--- Cubie/cubie.sql
+++ Cubie/cubie.sql
@@ -11,7 +11,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- 数据库: `icubie`
+-- 数据库: `cubie`
 --
 CREATE DATABASE IF NOT EXISTS `icubie` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `cubie`;
